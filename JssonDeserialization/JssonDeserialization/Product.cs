eea4e4c3fa38930fdf3097b7da1d4d61b6c997a2
ShawetaKumar <49275993+ShawetaKumar@users.noreply.github.com>
2020-07-14 13:49:03 +0530

    Add files via upload

---
+++ JssonDeserialization/JssonDeserialization/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JssonDeserialization
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        public string Type { get; set; }
+        public string Summary { get; set; }
+    }
+}
---
+++ JssonDeserialization/JssonDeserialization/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using JsonApiSerializer;
+using JssonDeserialization;
+using Newtonsoft.Json;
+
+namespace JsonDeserialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var json = "{ \"data\": {  \"id\": \"1\",  \"type\" : \"product\",  \"attributes\" : {    \"name\": \"name\",    \"summary\": \"summary\"  } }}";
+
+                var jsonApiSerializerSettings = new JsonApiSerializerSettings
+                {
+                    MissingMemberHandling = MissingMemberHandling.Error,
+                };
+
+                var product = JsonConvert.DeserializeObject<Product>(json, jsonApiSerializerSettings);
+
+                Console.WriteLine($"Product id:{product.Id}");
+
+                json = "{  \"id\": \"1\",  \"type\" : \"product\",  \"name\": \"name\", \"summary\": \"summary\" }";
+
+                var jsonSerializerSettings = new JsonSerializerSettings
+                {
+                    MissingMemberHandling = MissingMemberHandling.Error,
+                };
+
+                product = JsonConvert.DeserializeObject<Product>(json, jsonSerializerSettings);
+
+                Console.WriteLine(product.Id);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
